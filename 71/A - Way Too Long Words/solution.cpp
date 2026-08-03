2eafb88bbfa0c51a6659f438e2e9edd60da117d4
Sudheer kumar <sudheerkumar8076@gmail.com>
2026-08-03 22:21:18 +0530

    Add Way Too Long Words [A] from Codeforces

---
+++ 71/A - Way Too Long Words/solution.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n;
+    cin >> n;
+ 
+    while(n--)
+    {
+        string s;
+        cin >> s;
+ 
+        if(s.length() <= 10)
+        {
+            cout << s << "
+";
+        }
+        else
+        {
+            cout << s[0]
+                 << s.length() - 2
+                 << s[s.length() - 1]
+                 << "
+";
+        }
+    }
+ 
+    return 0;
+}
